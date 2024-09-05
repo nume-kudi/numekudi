dda024f81f66bdcc9d49a9e2b2921da9ddd57ae8
nnkkd <t.n.mailaddmain@gmail.com>
2024-09-05 23:50:51 +0900

    fix: text

--- src/components/role.tsx
+++ src/components/role.tsx
@@ -18,7 +18,7 @@ const Role = () => {
         <section className="p-4  rounded-lg shadow-md">
           <h2 className="text-2xl font-semibold text-gray-800">SIer時代</h2>
           <p className="text-gray-700">
-            SIerとして画像処理に携わり、カメラや照明の選定、画像処理プログラムの作成に取り組みました。C言語ライブラリをRustやPythonから扱う方法を自力で学びました。
+            SIerをしていました。画像処理に携わり、カメラや照明の選定、画像処理プログラムの作成に取り組みました。C言語ライブラリをRustやPythonから扱う方法を自力で学びました。
           </p>
         </section>
 
@@ -26,8 +26,8 @@ const Role = () => {
         <section className="p-4  rounded-lg shadow-md">
           <h2 className="text-2xl font-semibold text-gray-800">Web開発</h2>
           <p className="text-gray-700">
-            退職後、Web開発に携わり、生成AIをテーマにしたToBプロダクトを開発。PoCと本開発ではJavaScript/TypeScript,
-            React, NestJS, Pythonを使用しました。
+            Web開発に携わり、生成AIをテーマにしたToBプロダクトを開発。PoCと本開発ではJavaScript/TypeScript,
+            Pythonを使用しました。
           </p>
         </section>
 
